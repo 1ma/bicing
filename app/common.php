7c240bda7e293e964a7dc6229086ca15cbdccbdb
Marcel Hernandez <e6990620@gmail.com>
2017-08-07 19:53:27 +0200

    set timezone on PDO client too

--- app/common.php
+++ app/common.php
@@ -2,9 +2,11 @@
 
 use UMA\BicingStats\Postgres\Mapper;
 
-date_default_timezone_set('Europe/Andorra');
-
 require_once __DIR__ . '/../vendor/autoload.php';
+
+
+define('BCN_TIMEZONE', 'Europe/Andorra');
+date_default_timezone_set(BCN_TIMEZONE);
 
 $cnt = new \Slim\Container(['settings' => require_once __DIR__ . '/settings.php']);
 
@@ -12,11 +14,15 @@ $cnt['paths.root'] = realpath(__DIR__ . '/..');
 $cnt['paths.datastore'] = $cnt['paths.root'] . '/var/data';
 
 $cnt[\PDO::class] = function ($cnt) {
-    return new \PDO($cnt['settings']['pdo']['dsn'], null, null, [
+    $pdo = new \PDO($cnt['settings']['pdo']['dsn'], null, null, [
         \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
         \PDO::ATTR_EMULATE_PREPARES => false,
         \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
     ]);
+
+    $pdo->exec(sprintf("SET TIMEZONE TO '%s'", BCN_TIMEZONE));
+
+    return $pdo;
 };
 
 $cnt[Mapper::class] = function ($cnt) {

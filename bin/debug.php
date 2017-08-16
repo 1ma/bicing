a3296af1c9c9776b113dde6125306b478a191eb1
Marcel Hernandez <e6990620@gmail.com>
2017-08-17 00:26:50 +0200

    now useless

--- bin/debug.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Slim\Container;
-use UMA\Bicing\API\Collector;
-
-/** @var Container $cnt */
-$cnt = require_once __DIR__ . '/../app/modes/cli.php';
-
-echo json_encode($cnt[Collector::class]());
